8de63ca9e91f3d8ddfd42a73169ffe76d4111aa8
vanhopld98 <vanhopld98@gmail.com>
2021-08-23 09:27:58 +0700

    selection sort

---
+++ src/SelectionSort.java
@@ -0,0 +1,28 @@
+public class SelectionSort {
+    static double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
+
+    public static void selectionSort(double[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            double currenMin = list[i];
+            int currenMinIndex = i;
+
+            for (int j = i + 1; j < list.length; j++) {
+                if (currenMin > list[j]) {
+                    currenMin = list[j];
+                    currenMinIndex = j;
+                }
+            }
+            if (currenMinIndex != i) {
+                list[currenMinIndex] = list[i];
+                list[i] = currenMin;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        selectionSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i] + " ");
+        }
+    }
+}
